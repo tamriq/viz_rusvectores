0d2579a352aed8a72a58768ee9cc6bd0fe9fc77c
Nastasia <lisitsinan@gmail.com>
2019-02-05 17:36:31 +0300

    Update associates, freq and strings

--- Adds and reviews/freq.js
+++ Adds and reviews/freq.js
@@ -1,35 +1,11 @@
-$(function() {
-    var checkbox = $('input[id=high]');
-
-    checkbox.click(function() {
-        if (this.checked) {
-            $("li.high").fadeIn('slow');
-        } else {
-            $("li.high").fadeOut('slow');
-        }
-    });
-});
-
-$(function() {
-    var checkbox = $('input[id=mid]');
-
-    checkbox.click(function() {
-        if (this.checked) {
-            $("li.mid").fadeIn('slow');
-        } else {
-            $("li.mid").fadeOut('slow');
-        }
-    });
-});
-
-$(function() {
-    var checkbox = $('input[id=low]');
-
-    checkbox.click(function() {
-        if (this.checked) {
-            $("li.low").fadeIn('slow');
-        } else {
-            $("li.low").fadeOut('slow');
-        }
-    });
+$(document).ready(function(){
+  $("#high").click(function(){
+    $("li.high").fadeToggle('slow');
+  });
+  $("#mid").click(function(){
+	$("li.mid").fadeToggle('slow');
+  });
+  $("#low").click(function(){
+	$("li.low").fadeToggle('slow');
+  });
 });
